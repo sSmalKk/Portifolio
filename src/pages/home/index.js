30b99e5d75a67fc56118147c5fbc52887f8fb1f5
sSmalKk <49993796+sSmalKk@users.noreply.github.com>
2023-09-18 15:59:23 -0300

    act

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -17,7 +17,7 @@ export const Home = () => {
         <div className="intro_sec d-block d-lg-flex align-items-center ">
           <div
             className="h_bg-image order-1 order-lg-2 h-100 "
-            style={{ backgroundImage: `url(${introdata.your_img_url})` }}
+            style={{ backgroundImage: `url("https://www.behance.net/gallery/180299107/AutoRetrato/modules/1018477627")` }}
           ></div>
           <div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center">
             <div className="align-self-center ">
