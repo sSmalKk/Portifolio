c4ea1bfc4935570ca1f1484325a362c1069a84c2
sSmalKk <49993796+sSmalKk@users.noreply.github.com>
2023-09-18 19:58:52 -0300

    update

--- src/components/languagetoggle.js
+++ src/components/languagetoggle.js
@@ -1,32 +1,30 @@
-import React, {useState } from "react";
-import { WiMoonAltWaningCrescent4 } from "react-icons/wi";
+import React, { useEffect, useState } from "react";
 
-// Declare selectedLanguage here and assign a default value
-const selectedLanguage = "en";
+const SelectedLanguage = () => {
+  // 1. Inicializa o estado 'language' com o valor armazenado em localStorage ou 'null' se não houver nada.
+  const [language, setLanguage] = useState(localStorage.getItem("selectedLanguage"));
 
-const Languagetoggle = () => {
-  // Use localStorage to store the selected language
-  const [language, setLanguage] = useState(() => {
-    // Check if a language preference is stored in localStorage
-    const storedLanguage = localStorage.getItem("selectedLanguage");
-    // If not found, default to the value of selectedLanguage
-    return storedLanguage || selectedLanguage;
-  });
-
-  // Handle language toggle
-  const onToggleLanguage = () => {
-    const newLanguage = language === 'pt' ? 'en' : 'pt';
-    setLanguage(newLanguage);
-    // Save the selected language in localStorage
-    localStorage.setItem("selectedLanguage", newLanguage);
+  // 2. Define a função 'toggleLanguage' que é chamada quando o elemento é clicado.
+  const toggleLanguage = () => {
+    // 3. Alterna entre os valores 'pt' (português) e 'en' (inglês) e atualiza o estado 'language'.
+    setLanguage(language === "pt" ? "en" : "pt");
+    // 4. Recarrega a página quando o botão for clicado.
+    window.location.reload();
   };
 
+  // 5. O useEffect é usado para realizar ações após a renderização do componente.
+  useEffect(() => {
+    // 6. Salva o valor de 'language' em localStorage sempre que ele mudar.
+    localStorage.setItem("selectedLanguage", language);
+  }, [language]);
+
+  // 7. Renderiza um elemento <div> com a classe "nav_ac" que chama a função 'toggleLanguage' quando clicado.
   return (
-    <div className="nav_ac" onClick={onToggleLanguage}>
-      <WiMoonAltWaningCrescent4 />
+    <div className="nav_ac" onClick={toggleLanguage}>
+      {/* 9. Renderiza o valor de 'language' dentro do componente */}
+      {language}
     </div>
   );
 };
 
-export default Languagetoggle;
-export { selectedLanguage };
+export default SelectedLanguage;
--- src/content_option.js
+++ src/content_option.js
@@ -1,12 +1,11 @@
 // In your JavaScript file
 import * as enData from './en.json';
 import * as esData from './pt.json';
-import { selectedLanguage } from './components/languagetoggle';
 // Determine the selected language (you can get this from user preferences)
-const Language = selectedLanguage; // Example: English
+const storedLanguage = localStorage.getItem("selectedLanguage");
 
 // Load the appropriate language data
-const languageData = Language === 'pt' ? esData : enData;
+const languageData = storedLanguage === 'pt' ? esData : enData;
 
 // Now you can use languageData to access your localized strings
 const { meta, dataabout, dataportfolio, worktimeline, skills, introdata, contactConfig, socialprofils, logotext, titles } = languageData;
